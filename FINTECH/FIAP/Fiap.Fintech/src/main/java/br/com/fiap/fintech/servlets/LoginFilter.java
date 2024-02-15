a304d993ba85f4229488827a71117c0c396bd7f8
Pedro Henrique de Jesus Santos <114628005+Impedrojesus@users.noreply.github.com>
2024-02-15 10:22:20 -0300

    Add files via upload

---
+++ FINTECH/FIAP/Fiap.Fintech/src/main/java/br/com/fiap/fintech/servlets/LoginFilter.java
@@ -0,0 +1,33 @@
+package br.com.fiap.fintech.servlets;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/login")
+public class LoginFilter extends HttpFilter implements Filter {
+	private static final long serialVersionUID = 1L;
+
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest request  = (HttpServletRequest) servletRequest;
+		String username = request.getParameter("username");
+
+		HttpSession session = request.getSession();
+		session.setAttribute("username", username);
+		
+		chain.doFilter(servletRequest, servletResponse);
+		return;
+	}
+}
